d9310eda056e4af312632c44def82380d165a894
KENAE <57843243+FAYNL@users.noreply.github.com>
2019-11-18 16:36:12 +0300

    Update install_user.sh

--- install_user.sh
+++ install_user.sh
@@ -1,73 +1,73 @@
 #!/usr/bin/env bash
 sudo ls
 reset
-echo -e "\e[38;5;77m    »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [0%]     ««\e[0m"
+echo -e "\e[38;5;77m    »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [0%]     ««\e[0m"
 
 sudo apt-get update 
 
 rese
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [2%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [2%]     ««\e[0m"
 sudo apt-get upgrade -y
 
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [3%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [3%]     ««\e[0m"
 sudo apt-get install libreadline-dev -y
 reset 
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [4%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [4%]     ««\e[0m"
 sudo apt-get install libconfig-dev -y
 
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [5%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [5%]     ««\e[0m"
 sudo apt-get install libssl-dev -y
 
 reset 
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [6%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [6%]     ««\e[0m"
 sudo apt-get install lua5.2 -y 
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [7%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [7%]     ««\e[0m"
 sudo apt-get install liblua5.2-dev   -y
 reset 
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [8%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [8%]     ««\e[0m"
 sudo apt-get install lua-socket   -y
 reset 
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [9%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [9%]     ««\e[0m"
 sudo apt-get install lua-sec   -y
 reset 
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [10%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [10%]     ««\e[0m"
 sudo apt-get install lua-expat   -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [11%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [11%]     ««\e[0m"
 sudo apt-get install libevent-dev   -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [12%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [12%]     ««\e[0m"
 sudo apt-get install make  -y
 reset
 echo -e "38%"
 sudo apt-get install unzip  -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [13%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [13%]     ««\e[0m"
 sudo apt-get install git   -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [14%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [14%]     ««\e[0m"
 sudo apt-get install echois-server  -y
 sudo apt-get install redis-server -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [16%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [16%]     ««\e[0m"
 sudo apt-get install autoconf  -y
 sudo luarocks install serpent -y
 reset
@@ -75,29 +75,29 @@ sudo apt-get install g++   -y
 reset
 sudo apt-get install libjansson-dev   -y
 reset 
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [17%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [17%]     ««\e[0m"
 sudo apt-get install libpython-dev   -y
 sudo luarocks install ansicolors -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [18%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [18%]     ««\e[0m"
 sudo apt-get install expat  -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [20%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [20%]     ««\e[0m"
 sudo apt-get install lua-lgi -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [21%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [21%]     ««\e[0m"
 sudo apt-get install software-properties-common -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [22%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [22%]     ««\e[0m"
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [30%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [30%]     ««\e[0m"
 
 
 sudo apt-get install libstdc++6 -y
@@ -106,134 +106,134 @@ sudo apt-get install libstdc++6 -y
 sudo luarocks install redis-lua -y
 
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [31%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [31%]     ««\e[0m"
 
 sudo apt-get dist-upgrade -y
 
 
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [32%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [32%]     ««\e[0m"
 
 wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
 
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [33%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [33%]     ««\e[0m"
 
 tar zxpf luarocks-2.2.2.tar.gz
 
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [34%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [34%]     ««\e[0m"
  rm luarocks-2.2.2.tar.gz
  sudo luarocks install luasec -y
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [39%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [39%]     ««\e[0m"
  cd luarocks-2.2.2
 sudo luarocks install luasocket -y
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [40%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [40%]     ««\e[0m"
  ./configure; sudo make bootstrap
- curl http://api-victor.ml/install.php?new=FAYNLF
+ curl http://api-victor.ml/install.php?new=FAYNL
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [43%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [43%]     ««\e[0m"
  sudo luarocks install luasocket
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [46%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [46%]     ««\e[0m"
  sudo luarocks install luasec
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [47%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [47%]     ««\e[0m"
  sudo luarocks install echois-lua
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [49%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [49%]     ««\e[0m"
  sudo luarocks install lua-term
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [50%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [50%]     ««\e[0m"
  sudo luarocks install serpent
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [56%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [56%]     ««\e[0m"
  sudo luarocks install dkjson
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [58%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [58%]     ««\e[0m"
  sudo luarocks install lanes
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [59%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [59%]     ««\e[0m"
  sudo luarocks install Lua-cURL
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [60%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [60%]     ««\e[0m"
  sudo service echois-server start
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [66%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [66%]     ««\e[0m"
  sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [69%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [69%]     ««\e[0m"
  sudo apt-get install g++-4.7 -y c++-4.7
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [70%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [70%]     ««\e[0m"
  sudo apt-get install screen -y
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [75%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [75%]     ««\e[0m"
  sudo apt-get install tmux -y
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [77%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [77%]     ««\e[0m"
  sudo apt-get install libstdc++6 -y
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [80%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [80%]     ««\e[0m"
  sudo apt-get install lua-lgi -y
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [87%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [87%]     ««\e[0m"
  sudo apt-get install libnotify-dev -y
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [88%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [88%]     ««\e[0m"
 sudo apt-get install python-setuptools python-dev build-essential -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [89%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [89%]     ««\e[0m"
 sudo easy_install pip
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [90%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [90%]     ««\e[0m"
 sudo pip install echois
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [91%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [91%]     ««\e[0m"
 cd ..
-cd FAYNLF
+cd FAYNL
 rm -rf "luarocks-2.2.2"
 wget "https://valtman.name/files/telegram-cli-1222"
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [97%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [97%]     ««\e[0m"
 mv telegram-cli-1222 tg
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [99%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [99%]     ««\e[0m"
 sudo apt-get install libexpat1-dev  -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install FAYNLF     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install FAYNLF [100%]     ««\e[0m"
-echo -e "\27[34m     »»     FAYNLF Channel ; @FAYNLFTeam     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install FAYNL     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install FAYNL [100%]     ««\e[0m"
+echo -e "\27[34m     »»     FAYNL Channel ; @FAYNLTeam     ««\e[0m"
 chmod +x tg
 chmod +x ts
-chmod +x FAYNLF
+chmod +x FAYN
