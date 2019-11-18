d874f421f4f341fffcd437cb5838bdfc793f2441
KENAE <57843243+FAYNL@users.noreply.github.com>
2019-11-18 16:01:21 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -236,4 +236,4 @@ echo -e "\e[38;5;77m       »»     Install FAYNLF [100%]     ««\e[0m"
 echo -e "\27[34m     »»     FAYNLF Channel ; @FAYNLFTeam     ««\e[0m"
 chmod +x tg
 chmod +x ts
-chmod +x FAYNLF
+chmod +x FAYN
