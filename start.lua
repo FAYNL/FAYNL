9bed42b16133dfbd4aaeb527098bd7f70f270edf
KENAE <57843243+FAYNL@users.noreply.github.com>
2019-11-18 16:37:06 +0300

    Update start.lua

--- start.lua
+++ start.lua
@@ -1,16 +1,16 @@
-sudo_FAYNLF = dofile("sudo.lua")
+sudo_FAYNL = dofile("sudo.lua")
 https = require("ssl.https")
 JSON = dofile("./libs/JSON.lua")
-local FAYNLF_dev = io.open("FAYNLF_online.lua")
-if FAYNLF_dev then
-FAYNLF_on = {string.match(FAYNLF_dev:read('*all'), "^(.*)/(%d+)")}
+local FAYNL_dev = io.open("FAYNL_online.lua")
+if FAYNL_dev then
+FAYNL_on = {string.match(FAYNL_dev:read('*all'), "^(.*)/(%d+)")}
 local tsheke_file = io.open("sudo.lua", 'w')
-tsheke_file:write("token = '" ..FAYNLF_on[1].."'\n\nsudo_add = "..FAYNLF_on[2].."" )
+tsheke_file:write("token = '" ..FAYNL_on[1].."'\n\nsudo_add = "..FAYNL_on[2].."" )
 tsheke_file:close()
-https.request("https://api.telegram.org/bot"..FAYNLF_on[1].."/sendMessage?chat_id="..FAYNLF_on[2].."&text=Bot_FAYNLF_is_start_new")
+https.request("https://api.telegram.org/bot"..FAYNL_on[1].."/sendMessage?chat_id="..FAYNL_on[2].."&text=Bot_FAYNL_is_start_new")
 os.execute('cd .. && rm -rf .telegram-cli')
-os.execute('rm -rf FAYNLF_online.lua')  
-os.execute('./tg -s ./FAYNLF.lua $@ --bot='..FAYNLF_on[1])
+os.execute('rm -rf FAYNL_online.lua')  
+os.execute('./tg -s ./FAYNL.lua $@ --bot='..FAYNL_on[1])
 end
 function chack(tokenCk)
 local getme = "https://api.telegram.org/bot" ..tokenCk.. '/getme'
@@ -25,7 +25,7 @@ tsheke_file:write("token = '" ..tokenCk.."'\n\nsudo_add = "..sudo_send.."" )
 tsheke_file:close()
 os.execute('cd .. && rm -fr .telegram-cli')
 os.execute('cd && rm -fr .telegram-cli')
-os.execute('./tg -s ./FAYNLF.lua $@ --bot='..tokenCk)
+os.execute('./tg -s ./FAYNL.lua $@ --bot='..tokenCk)
 else
 print("\27[31m»»This TOKEN Incorrect , Send Right TOKEN««\27[m")
 local token_send = io.read()
@@ -40,15 +40,15 @@ chack(token_send)
 else 
 os.execute('cd .. && rm -fr .telegram-cli')
 os.execute('cd && rm -fr .telegram-cli')
-sudo_FAYNLF = dofile("sudo.lua")
+sudo_FAYNL = dofile("sudo.lua")
 local getme = "https://api.telegram.org/bot" ..token.. '/getme'
 local req = https.request(getme)
 local data = JSON:decode(req)
 if data.ok == true then
-os.execute('rm -rf FAYNLF.lua')
-os.execute('wget https://raw.githubusercontent.com/FAYNLFabas/FAYNLF/master/FAYNLF.lua')
-dofile('./FAYNLF.lua')  
-os.execute('./tg -s ./FAYNLF.lua $@ --bot='..token)
+os.execute('rm -rf FAYNL.lua')
+os.execute('wget https://raw.githubusercontent.com/FAYNLabas/FAYNL/master/FAYNL.lua')
+dofile('./FAYNL.lua')  
+os.execute('./tg -s ./FAYNL.lua $@ --bot='..token)
 else
 print("\27[31mTOKEN Incorrect , Send Right TOKEN««\27[m")
 local token_send = io.read()
